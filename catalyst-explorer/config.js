87361320f1ac7d5a125fa3adf40e6e20c2f34127
n8tb1t <n8tb1t@gmail.com>
2019-08-29 17:04:28 +0500

    feat: add explorer

---
+++ catalyst-explorer/config.js
@@ -0,0 +1,7 @@
+var api = 'https://cxex.catalystcrypto.net/cxex-explorer';
+
+var donationAddress = "";
+var blockTargetInterval = 60;
+var coinUnits = 10000000;
+var symbol = 'CX';
+var refreshDelay = 5000;
