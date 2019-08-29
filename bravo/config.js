a5e6a1b98935691992698977d3aef5d77cb5bcd9
n8tb1t <n8tb1t@gmail.com>
2019-08-29 16:27:26 +0500

    feat: add pools.

---
+++ bravo/config.js
@@ -0,0 +1,18 @@
+var api = "https://bravo.cryptocatalyst.net/cat";
+var apiMerged = "https://bravo.cryptocatalyst.net/btcm";
+
+var email = "n8tb1t@gmail.com";
+var telegram = "";
+var discord = "https://discord.gg/byKYDyb";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var blockchainExplorer = "https://www.cx-explorer.gq/index.html?hash={id}#blockchain_block";
+var transactionExplorer = "https://www.cx-explorer.gq/index.html?hash={id}#blockchain_transaction";
+
+
+var blockchainExplorerMerged = "http://explorer.bitcoinmono.io/?hash={id}#blockchain_block";
+var transactionExplorerMerged = "http://explorer.bitcoinmono.io/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
